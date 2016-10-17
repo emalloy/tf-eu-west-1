4863fda572d2be1cb5641c10ab1d8ac6db33fc5d
Eric Malloy <malloy.j.eric@gmail.com>
2016-10-17 18:27:03 -0400

    enable versioning for s3 bucket

--- eu-west-1/aws/s3/s3.tf
+++ eu-west-1/aws/s3/s3.tf
@@ -2,4 +2,7 @@ resource "aws_s3_bucket" "bucketname-eu-west-1" {
     bucket = "${var.project}-${var.region}"
     acl    = "private"
     region = "eu-west-1"
+    versioning {
+        enabled = true
+    }
 }
